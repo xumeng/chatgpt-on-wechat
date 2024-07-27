0db8c15fc9cef4ff2572b55e85341237ef528bf5
Meng <gosuxm@gmail.com>
2024-07-27 16:28:25 +0800

    update

--- plugins/hello/hello.py
+++ plugins/hello/hello.py
@@ -17,13 +17,11 @@ from config import conf
     version="0.1",
     author="lanvent",
 )
-
-
 class Hello(Plugin):
 
-    group_welc_prompt = "请你随机使用一种风格说一句问候语来欢迎新用户\"{nickname}\"加入群聊。"
-    group_exit_prompt = "请你随机使用一种风格介绍你自己，并告诉用户输入#help可以查看帮助信息。"
-    patpat_prompt = "请你随机使用一种风格跟其他群用户说他违反规则\"{nickname}\"退出群聊。"
+    group_welc_prompt = '请你随机使用一种风格说一句问候语来欢迎新用户"{nickname}"加入群聊。'
+    patpat_prompt = "请你随机使用一种风格介绍你自己，并告诉用户输入#help可以查看帮助信息。"
+    group_exit_prompt = '请你随机使用一种风格跟其他群用户说他违反规则"{nickname}"退出群聊。'
 
     def __init__(self):
         super().__init__()
@@ -42,12 +40,7 @@ class Hello(Plugin):
             raise "[Hello] init failed, ignore "
 
     def on_handle_context(self, e_context: EventContext):
-        if e_context["context"].type not in [
-            ContextType.TEXT,
-            ContextType.JOIN_GROUP,
-            ContextType.PATPAT,
-            ContextType.EXIT_GROUP
-        ]:
+        if e_context["context"].type not in [ContextType.TEXT, ContextType.JOIN_GROUP, ContextType.PATPAT, ContextType.EXIT_GROUP]:
             return
         msg: ChatMessage = e_context["context"]["msg"]
         group_name = msg.from_user_nickname
@@ -68,7 +61,7 @@ class Hello(Plugin):
             if not self.config or not self.config.get("use_character_desc"):
                 e_context["context"]["generate_breaked_by"] = EventAction.BREAK
             return
-        
+
         if e_context["context"].type == ContextType.EXIT_GROUP:
             if conf().get("group_chat_exit_group"):
                 e_context["context"].type = ContextType.TEXT
@@ -77,7 +70,7 @@ class Hello(Plugin):
                 return
             e_context.action = EventAction.BREAK
             return
-            
+
         if e_context["context"].type == ContextType.PATPAT:
             e_context["context"].type = ContextType.TEXT
             e_context["context"].content = self.patpat_prompt
